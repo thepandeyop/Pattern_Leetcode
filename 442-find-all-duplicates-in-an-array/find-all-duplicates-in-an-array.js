ea08b4bd7ab3ba840e20e10232003b412ce387cd
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-07-06 18:09:37 +0530

    Time: 138 ms (9.24%) | Memory: 58.6 MB (73.36%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -4,11 +4,13 @@
  */
 var findDuplicates = function(nums) {
     let res = new Array();
-    nums = nums.sort((a,b)=>a-b);
-    for(let i=0;i<nums.length;i++){
+    nums = nums.sort((a,b)=>a-b); // time complexity = O(nlogn);
+    for(let i=0;i<nums.length;i++){    //O(n);
         if(nums[i-1]===nums[i]){
             res.push(nums[i]);
         }
     }
     return res;
 };
+// overall time complexity: O(n) + O(nlogn);
+            // space complexity : O(n);
