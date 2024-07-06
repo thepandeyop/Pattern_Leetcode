d23b37e6d7f7bbc698266d0ee80808cb16ea5edc
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-07-06 13:38:44 +0530

    Time: 189 ms (36.70%) | Memory: 71.2 MB (56.38%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} original
+ * @param {number} m
+ * @param {number} n
+ * @return {number[][]}
+ */
+var construct2DArray = function(original, m, n) {
+    if(m*n!= original.length){
+        return [];
+    }
+    let arr = [];
+    for(let i=0,x=0;i<m;i++){
+        arr[i] =[];
+        for(let j=0;j<n;j++){
+            arr[i][j] = original[x++];
+        }
+    }
+    return arr;
+    
+};
