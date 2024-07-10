198911ecc9629826e59f23e4f5624c7e9a23b3c1
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-07-10 15:34:46 +0530

    Time: 85 ms (53.95%) | Memory: 56.5 MB (38.20%) - LeetSync

---
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+      for (let i = 0; i < nums.length; i++) {
+        nums[i] = Math.abs(nums[i]);
+    }
+    nums.sort((a,b)=>a-b);
+    for(let i=0;i<nums.length;i++){
+        nums[i]=nums[i]*nums[i]
+    }
+    return nums;
+};
