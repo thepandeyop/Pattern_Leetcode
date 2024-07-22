b14411a0afc2149a504f361671a5a890503b88f7
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-07-22 22:43:33 +0530

    Time: 48 ms (96.54%) | Memory: 51.8 MB (13.60%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,10 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   for(let i =0;i<nums.length;i++){
-       for(let j =i+1;j<nums.length;j++){
-           if(nums[i]+nums[j]===target)return [i,j];
-       }
-   }
-    return -1;
+ let map = new Map();
+    
+    for(let i=0;i<nums.length; i++){
+       const  sub =  target - nums[i];
+        if(map.has(sub)){
+            return [map.get(sub),i];
+        }
+        map.set(nums[i],i);
+    }
+    return null;
 };
