40a51e9b3431c4a682ec2dfdebde3718fd30086d
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-08-01 15:04:11 +0530

    Time: 139 ms (30.02%) | Memory: 78.1 MB (13.02%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.js
+++ 647-palindromic-substrings/palindromic-substrings.js
@@ -2,21 +2,26 @@
  * @param {string} s
  * @return {number}
  */
-function pal(s,i,j){
+function pal(s,i,j,dp){
     if(i>=j){
         return true;
     }
-    if(s[i]===s[j]){
-       return pal(s,i+1,j-1);
+    if(dp[i][j]!==false)return dp[i][j]
+    
+     if (s[i] === s[j] && pal(s, i + 1, j - 1, dp)) {
+        dp[i][j] = true;
+    } else {
+        dp[i][j] = false;
     }
-    return false;
+    return dp[i][j];
 }
 var countSubstrings = function(s) {
     let count = 0;
     let n = s.length;
+    let dp = new Array(n).fill().map(()=>Array(n).fill(false));
     for(let i=0;i<s.length;i++){
         for(let j=i;j<s.length;j++){
-            if(pal(s,i,j)===true){
+            if(pal(s,i,j,dp)===true){
                 count ++;
             }
         }
