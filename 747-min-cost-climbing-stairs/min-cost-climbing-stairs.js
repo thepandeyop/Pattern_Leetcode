3425c57ac8f55be97a2e6b8432b179b836dea10c
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-08-01 08:31:13 +0530

    Time: 60 ms (38.99%) | Memory: 50.5 MB (40.52%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+
+function solve(i, cost, dp) {
+    if (i >= cost.length) {
+        return 0;
+    }
+    if (dp[i] !== -1) return dp[i];
+    
+    let a = cost[i] + solve(i + 1, cost, dp);
+    let b = cost[i] + solve(i + 2, cost, dp);
+    
+    dp[i] = Math.min(a, b);
+    return dp[i];
+}
+
+var minCostClimbingStairs = function(cost) {
+    let n = cost.length;
+    let dp = new Array(n + 1).fill(-1);
+    return Math.min(solve(0, cost, dp), solve(1, cost, dp));
+};
