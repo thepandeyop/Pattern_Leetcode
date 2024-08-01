1765e6410a9e6d029c4c1509971aad258ba39f28
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-08-01 07:42:32 +0530

    Time: 44 ms (93.67%) | Memory: 48.9 MB (29.51%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -1,20 +1,17 @@
 /**
  * @param {number} n
  * @return {number}
- */
-function solve(n, dp) {
-    if (n <= 1) return n;
-
-    if (dp[n] !== -1) return dp[n];
-
-    dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-    return dp[n];
-}
+ **/
 
 var fib = function(n) {
     if (n <= 1) {
         return n;
     }
     let dp = new Array(n + 1).fill(-1);
-    return solve(n, dp);
+    dp[0] = 0;
+    dp[1] = 1;
+    for(let i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    } 
+    return dp[n];
 };
