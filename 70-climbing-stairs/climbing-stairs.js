a5e29445a4ae65dba3f6ec54f921d7a251c7657e
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-08-01 08:01:31 +0530

    Time: 53 ms (44.90%) | Memory: 48.6 MB (71.76%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function solve(n, dp) {
+    if (n <= 1) return 1;
+
+    if (dp[n] !== -1) return dp[n];
+
+    dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    return dp[n];
+}
+
+var climbStairs = function(n) {
+    if (n <= 1) return 1;
+
+    let dp = new Array(n + 1).fill(-1);
+    return solve(n, dp);
+};
