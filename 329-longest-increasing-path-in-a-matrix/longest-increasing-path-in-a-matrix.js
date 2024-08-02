00c9f67cec3fad2c54318c955e5e9b2e06820051
Abhishek Pandey <116102773+thepandeyop@users.noreply.github.com>
2024-08-02 17:20:08 +0530

    Time: 74 ms (87.55%) | Memory: 51.9 MB (99.17%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.js
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.js
@@ -1,39 +1,37 @@
-/**
- * @param {number[][]} matrix
- * @return {number}
- */
-var longestIncreasingPath = function(matrix) {
-     const m = matrix.length;
+function longestIncreasingPath(matrix) {
+    const m = matrix.length;
     const n = matrix[0].length;
-    const dp = Array.from({ length: m }, () => Array(n).fill(0));
+    const dp = Array.from({ length: m }, () => new Array(n).fill(0));
 
     let res = 0;
-
-    function dfs(i, j, prevVal) {
-        if (i < 0 || i >= m || j < 0 || j >= n || matrix[i][j] <= prevVal) {
-            return 0;
-        }
-
-        if (dp[i][j] !== 0) {
-            return dp[i][j];
-        }
-
-        let temp = 0;
-        const curr = matrix[i][j];
-        temp = Math.max(temp, dfs(i + 1, j, curr));
-        temp = Math.max(temp, dfs(i - 1, j, curr));
-        temp = Math.max(temp, dfs(i, j + 1, curr));
-        temp = Math.max(temp, dfs(i, j - 1, curr));
-
-        dp[i][j] = ++temp;
-        return dp[i][j];
-    }
-
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            res = Math.max(dfs(i, j, Number.MIN_SAFE_INTEGER), res);
+            res = Math.max(dfs(matrix, i, j, -Infinity, dp), res);
         }
     }
 
     return res;
-};
+}
+
+function dfs(matrix, i, j, prevVal, dp) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    if (i < 0 || i >= m || j < 0 || j >= n || matrix[i][j] <= prevVal) {
+        return 0;
+    }
+
+    if (dp[i][j] !== 0) {
+        return dp[i][j];
+    }
+
+    let temp = 0;
+    const curr = matrix[i][j];
+    temp = Math.max(temp, dfs(matrix, i + 1, j, curr, dp));
+    temp = Math.max(temp, dfs(matrix, i - 1, j, curr, dp));
+    temp = Math.max(temp, dfs(matrix, i, j + 1, curr, dp));
+    temp = Math.max(temp, dfs(matrix, i, j - 1, curr, dp));
+
+    dp[i][j] = ++temp;
+    return dp[i][j];
+}
